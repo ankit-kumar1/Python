d7585e4ffbd64ed2246aa92f8914c7f8e41a7b6c
Ankit Kumar <35533136+ankit-kumar1@users.noreply.github.com>
2019-08-27 14:31:26 -0400

    Update sorting1.py

--- codingpractise/sorting1.py
+++ codingpractise/sorting1.py
@@ -1,3 +1,8 @@
+"""
+All sorted lowercase letters are ahead of uppercase letters.
+All sorted uppercase letters are ahead of digits.
+All sorted odd digits are ahead of sorted even digits."""
+
 s = input()
 odd=[]
 lower=[]
